b3c646f30b1e66939482b781db7aca9cd1f6df99
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2023-04-12 16:16:41 -0500

    added spring intialized project

---
+++ coachutd/src/main/java/com/groupfour/coachutd/CoachutdApplication.java
@@ -0,0 +1,13 @@
+package com.groupfour.coachutd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachutdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachutdApplication.class, args);
+	}
+
+}
---
+++ coachutd/src/test/java/com/groupfour/coachutd/CoachutdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupfour.coachutd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachutdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
