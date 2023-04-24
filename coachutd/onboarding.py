44e3154e1181fc6cfbe87360b5d7387bef38aefc
Amrit Rathie <amrit.rathie@gmail.com>
2023-04-24 17:05:19 -0500

    make onboarding post endpoint simpler

--- coachutd/onboarding.py
+++ coachutd/onboarding.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request
-from . import db
 from .models import User
 
 onboarding = Blueprint('onboarding', __name__)
@@ -13,9 +12,11 @@ def set_personal_information():
     sex = request.form.get('sex')
     availability = request.form.get('availability')
 
-    User.query.filter_by(username=username).first().age = age
-    User.query.filter_by(username=username).first().sex = sex
-    User.query.filter_by(username=username).first().availability = availability
+    user = User.query.filter_by(username=username).one()
+    user.age = age
+    user.sex = sex
+    user.availability = availability
+
 
 
 
