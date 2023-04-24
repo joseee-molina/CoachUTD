5b7815ebc5acc2a4be8ec7aaa42a32edb2f7792c
Amrit Rathie <amrit.rathie@gmail.com>
2023-04-24 16:59:10 -0500

    register onboarding controller

--- coachutd/__init__.py
+++ coachutd/__init__.py
@@ -4,25 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config["SECRET_KEY"] = "secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
 
-    # blueprint for auth routes in our app
+    # register controllers
     from .auth import auth as auth_blueprint
+
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
     from .profile import profile as profile_blueprint
+
     app.register_blueprint(profile_blueprint)
 
+    from .onboarding import onboarding as onboarding_blueprint
+
+    app.register_blueprint(onboarding_blueprint)
 
     return app
-
