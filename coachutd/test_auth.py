603aeefc0b11fdfb7e6f8b9cc694bed96ad22ddf
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2023-04-24 10:24:43 -0500

    added test cases for auth

--- coachutd/test_auth.py
+++ coachutd/test_auth.py
@@ -19,3 +19,37 @@ def test_login_correct():
                                                     password="test"))
         assert response.status_code == 200
 
+def test_login_valid_valid():
+    client = get_test_client()
+
+    with client.application.app_context():
+        # create a user
+        db.session.add(User(username="Jonathan", password="Hello123"))
+
+        response = client.post( '/login', data=dict(username="Jonathan",
+                                                    password="Hello123"))
+        assert response.status_code == 200
+
+
+def test_login_valid_invalid():
+    client = get_test_client()
+
+    with client.application.app_context():
+        # create a user
+        db.session.add(User(username="Jonathan", password="Hello123"))
+
+        response = client.post( '/login', data=dict(username="Jonathan",
+                                                    password="Hell123"))
+        assert response.status_code == 403
+
+def test_login_invalid_valid():
+    client = get_test_client()
+
+    with client.application.app_context():
+        # create a user
+        db.session.add(User(username="Jonatan", password="Hello123"))
+
+        response = client.post( '/login', data=dict(username="Jonathan",
+                                                    password="Hell123"))
+        assert response.status_code == 403
+
