02c378faf46340dca93b72d58df116e37d506d81
Amrit Rathie <amrit.rathie@gmail.com>
2023-04-24 23:37:28 -0500

    add email to user model

--- coachutd/models.py
+++ coachutd/models.py
@@ -4,9 +4,10 @@ from . import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    email = db.Column(db.Text, unique=True)
+    username = db.Column(db.Text, unique=True)
+    password = db.Column(db.Text)
     about = db.Column(db.Text)
     age = db.Column(db.Integer)
-    sex = db.Column(db.String(100))
-    availability = db.Column(db.String(100))
+    sex = db.Column(db.String(2))
+    availability = db.Column(db.String(9))
