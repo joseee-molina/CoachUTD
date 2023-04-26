960291b8e80512dbca5b4fc71f3f81ed8d861721
Amrit Rathie <amrit.rathie@gmail.com>
2023-04-26 17:05:28 -0500

    add `Post` and `Like` models

--- coachutd/models.py
+++ coachutd/models.py
@@ -11,3 +11,15 @@ class User(db.Model, UserMixin):
     age = db.Column(db.Integer)
     sex = db.Column(db.Text)
     availability = db.Column(db.Text)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.ForeignKey("user.id"))
+    body = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class Like(db.Model):
+    user = db.Column(db.ForeignKey("user.id"), primary_key=True)
+    post = db.Column(db.ForeignKey("post.id"), primary_key=True)
